5f0edf4113974a9f3d676327a55be21ce5736aeb
anasapra <79312148+anasapra@users.noreply.github.com>
2022-04-03 14:32:54 +0300

    Add files via upload
    не разобралась, как именно грузить файлы проекта, архивом или поштучно

---
+++ AppTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Второй тест");
+    }
+}
